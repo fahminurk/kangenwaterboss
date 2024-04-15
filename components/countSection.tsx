386c13e34530645e675b78031e14a6ab2c8504ac
Fahmi Nurkamil <fahminurk31@gmail.com>
2024-04-15 21:17:10 +0700

    p

--- components/countSection.tsx
+++ components/countSection.tsx
@@ -2,57 +2,57 @@
 import { counts } from "@/constants";
 import React, { useState } from "react";
 import CountUp from "react-countup";
-import ScrollTrigger from "react-scroll-trigger";
+import { useInView } from "react-intersection-observer";
 
 const CountSection = () => {
   const [counterOn, setCounterOn] = useState<boolean>(false);
+  const [ref, inView] = useInView();
+
+  React.useEffect(() => {
+    if (inView) {
+      setCounterOn(true);
+    } else {
+      setCounterOn(false);
+    }
+  }, [inView]);
 
   return (
-    <ScrollTrigger
-      onEnter={() => setCounterOn(true)}
-      onExit={() => setCounterOn(false)}
+    <div
+      ref={ref}
+      className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4"
     >
-      <div className="">
-        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-          {counts.map((item) => (
-            <div
-              key={item.title}
-              className="flex flex-col gap-5 justify-center items-center p-10 text-center text-white rounded-md bg-gradient-to-b from-white via-blue-500 to-blue-800"
-            >
-              <div className="flex items-center gap-2 ">
-                {item.count1 > 0 && (
-                  <>
-                    <p className="font-bold text-4xl">
-                      {counterOn && (
-                        <CountUp
-                          start={0}
-                          end={item.count1}
-                          duration={2}
-                          delay={0}
-                        />
-                      )}
-                    </p>
-                    <p>-</p>
-                  </>
-                )}
+      {counts.map((item) => (
+        <div
+          key={item.title}
+          className="flex flex-col gap-5 justify-center items-center p-10 text-center text-white rounded-md bg-gradient-to-b from-white via-blue-500 to-blue-800"
+        >
+          <div className="flex items-center gap-2 ">
+            {item.count1 > 0 && (
+              <>
                 <p className="font-bold text-4xl">
                   {counterOn && (
                     <CountUp
                       start={0}
-                      end={item.count2}
+                      end={item.count1}
                       duration={2}
                       delay={0}
                     />
                   )}
                 </p>
-                <p>{item.title}</p>
-              </div>
-              <p>{item.desc}</p>
-            </div>
-          ))}
+                <p>-</p>
+              </>
+            )}
+            <p className="font-bold text-4xl">
+              {counterOn && (
+                <CountUp start={0} end={item.count2} duration={2} delay={0} />
+              )}
+            </p>
+            <p>{item.title}</p>
+          </div>
+          <p>{item.desc}</p>
         </div>
-      </div>
-    </ScrollTrigger>
+      ))}
+    </div>
   );
 };
 
