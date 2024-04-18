7c100215da04d29d041198003145b83844277eeb
Fahmi Nurkamil <78724265+fahminurk@users.noreply.github.com>
2024-04-19 01:31:24 +0700

    Update ProductSidebar.tsx

--- components/ProductSidebar.tsx
+++ components/ProductSidebar.tsx
@@ -1,10 +1,1 @@
-import { Product } from "@/app/product/[name]/page";
-import React from "react";
 
-const ProductSidebar: React.FC<{ item: Product }> = ({ item }) => {
-  console.log(item);
-
-  return <div>s</div>;
-};
-
-export default ProductSidebar;
