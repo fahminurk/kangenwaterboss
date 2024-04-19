373fd5e7f2ef5db47228e3950154b3ade7891286
Fahmi Nurkamil <fahminurk31@gmail.com>
2024-04-19 15:27:03 +0700

    p

--- components/WhatsappToggle.tsx
+++ components/WhatsappToggle.tsx
@@ -34,22 +34,10 @@ const WhatsappToggle = () => {
         </div>
         <FaSquareWhatsapp
           onClick={() => setChat(!chat)}
-          className="text-5xl right-0 md:text-7xl text-green-500 cursor-pointer z-30"
+          className="text-5xl bg-white rounded-lg right-0 md:text-7xl text-green-500 cursor-pointer z-30"
         />
       </div>
     </div>
-    // <a
-
-    //   className="fixed bottom-0 m-4 right-0 group"
-    // >
-    //   <div
-    //     style={{ writingMode: "vertical-rl" }}
-    //     className="bg-white w-full flex justify-center items-center py-5 md:text-4xl font-bold rotate-180 rounded-b-lg group-hover:opacity-100 opacity-0 transition-all translate-y-10 group-hover:-translate-y-1"
-    //   >
-    //     chat
-    //   </div>
-    //   <FaSquareWhatsapp className="text-5xl md:text-7xl text-green-500 cursor-pointer zoom-in-90" />
-    // </a>
   );
 };
 
